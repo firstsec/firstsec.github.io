4f7ca996ca46362b8c2490f35bedb51b1bfde7bf
firstsec <firstsec@tuta.com>
2026-04-13 17:59:09 +0300

    fix script

--- analytics.js
+++ analytics.js
@@ -1,6 +1,3 @@
-
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-D3HH5Q3XYY"></script>
-
 window.dataLayer = window.dataLayer || [];
 function gtag() { dataLayer.push(arguments); }
 gtag('js', new Date());
