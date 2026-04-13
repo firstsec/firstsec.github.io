c83b4783961a2b761dbfb5f6527015ad186635bf
firstsec <firstsec@tuta.com>
2026-04-13 17:54:02 +0300

    add analytics

---
+++ analytics.js
@@ -0,0 +1,8 @@
+
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-D3HH5Q3XYY"></script>
+
+window.dataLayer = window.dataLayer || [];
+function gtag() { dataLayer.push(arguments); }
+gtag('js', new Date());
+
+gtag('config', 'G-D3HH5Q3XYY');
